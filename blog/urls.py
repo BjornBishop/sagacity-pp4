dea9b3a9850739a93ad8896c716dfe5e780b7bb8
Bjorn Bishop <bjornbishop@gmail.com>
2025-01-31 21:01:27 +0000

    removed models from views.py, replaced with queryset

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from blog.views import ConsultingAssignmentList
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('assignments/', ConsultingAssignmentList.as_view(), name='assignment_list'), 
+    path('assignments/', ConsultingAssignmentList.as_view(), name='assignment_list'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,6 @@ from .models import ConsultingAssignment
 
 # Create your views here.
 class ConsultingAssignmentList(generic.ListView):
-    model = ConsultingAssignment
+    queryset = ConsultingAssignment.objects.all()
     template_name = 'assignments/post_list.html'  
     context_object_name = 'assignments'  
