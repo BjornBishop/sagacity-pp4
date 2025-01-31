3ceda9fb09138774af3f6c428a8fe1b71bb7f143
Bjorn Bishop <bjornbishop@gmail.com>
2025-01-31 19:11:22 +0000

    added comments to admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,21 @@
 from django.contrib import admin
-from .models import Post
+from .models import ConsultingAssignment
 
 # Register your models here.
 
-@admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'status', 'created_on')
-    list_filter = ('status',)
-    search_fields = ('title', 'content')
+@admin.register(ConsultingAssignment)
+class ConsultingAssignmentAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'status', 'created_on', 'industry')
+    list_filter = ('status', 'industry')
+    search_fields = ('title', 'required_experience', 'role_description')
     prepopulated_fields = {'slug': ('title',)}
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('author', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('author__username', 'content')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
