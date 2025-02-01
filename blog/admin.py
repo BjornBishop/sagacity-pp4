d4937107a61afa72cb7f706f7772b429d496c4e1
Bjorn Bishop <bjornbishop@gmail.com>
2025-02-01 15:30:27 +0000

    base.html and index.html creates and populated

--- blog/admin.py
+++ blog/admin.py
@@ -1,22 +1,25 @@
 from django.contrib import admin
 from .models import ConsultingAssignment, Comment
+from django_summernote.admin import SummernoteModelAdmin
+
 
 # Register your models here.
 
 @admin.register(ConsultingAssignment)
-class ConsultingAssignmentAdmin(admin.ModelAdmin):
+class ConsultingAssignmentAdmin(SummernoteModelAdmin): 
     list_display = ('title', 'slug', 'status', 'created_on', 'industry')
     list_filter = ('status', 'industry')
     search_fields = ('title', 'required_experience', 'role_description')
     prepopulated_fields = {'slug': ('title',)}
-
+    summernote_fields = ('required_experience', 'role_description') 
 
 @admin.register(Comment)
-class CommentAdmin(admin.ModelAdmin):
+class CommentAdmin(SummernoteModelAdmin):
     list_display = ('author', 'post', 'created_on', 'approved')
     list_filter = ('approved', 'created_on')
-    search_fields = ('author__username', 'content')
+    search_fields = ('author__username', 'body')
     actions = ['approve_comments']
+    summernote_fields = ('body',) 
 
     def approve_comments(self, request, queryset):
         queryset.update(approved=True)
