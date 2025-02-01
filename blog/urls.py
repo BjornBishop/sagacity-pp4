7fe0f1ab8d76b2253a7bb33787132155b940738a
Bjorn Bishop <bjornbishop@gmail.com>
2025-02-01 16:37:47 +0000

    added static/css/style.css and connected it to base.html

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from blog.views import ConsultingAssignmentList
+from blog.views import ConsultingAssignmentList, ConsultingAssignmentDetail
 
 urlpatterns = [
     path('', ConsultingAssignmentList.as_view(), name='home'),
+    path('assignment/<slug:slug>/', ConsultingAssignmentDetail.as_view(), name='assignment_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import ConsultingAssignment
 
-# Create your views here.
 class ConsultingAssignmentList(generic.ListView):
     queryset = ConsultingAssignment.objects.filter(status__in=[1, 2])  # Filter out Draft (0) and Closed (3)
-    template_name = 'assignments/index.html' 
+    template_name = 'assignments/index.html'
     context_object_name = 'assignments'
-    paginate_by = 6  
+    paginate_by = 6
+
+class ConsultingAssignmentDetail(generic.DetailView):
+    model = ConsultingAssignment
+    template_name = 'assignments/detail.html'
