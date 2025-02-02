ffdaadf84c4f34e858cde7a854cf5f803343a98b
Bjorn Bishop <bjornbishop@gmail.com>
2025-02-02 13:10:04 +0000

    aligning text in assignment details

--- blog/admin.py
+++ blog/admin.py
@@ -1,25 +1,17 @@
 from django.contrib import admin
 from .models import ConsultingAssignment, Comment
-from django_summernote.admin import SummernoteModelAdmin
 
-
-# Register your models here.
-
-@admin.register(ConsultingAssignment)
-class ConsultingAssignmentAdmin(SummernoteModelAdmin): 
-    list_display = ('title', 'slug', 'status', 'created_on', 'industry')
-    list_filter = ('status', 'industry')
+class ConsultingAssignmentAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_on', 'status')
     search_fields = ('title', 'required_experience', 'role_description')
+    list_filter = ('status', 'created_on', 'industry')
     prepopulated_fields = {'slug': ('title',)}
-    summernote_fields = ('required_experience', 'role_description') 
 
-@admin.register(Comment)
-class CommentAdmin(SummernoteModelAdmin):
+class CommentAdmin(admin.ModelAdmin):
     list_display = ('author', 'post', 'created_on', 'approved')
     list_filter = ('approved', 'created_on')
-    search_fields = ('author__username', 'body')
-    actions = ['approve_comments']
-    summernote_fields = ('body',) 
+    search_fields = ('author__username', 'content')
 
-    def approve_comments(self, request, queryset):
-        queryset.update(approved=True)
+# Register your models here
+admin.site.register(ConsultingAssignment, ConsultingAssignmentAdmin)
+admin.site.register(Comment, CommentAdmin)
