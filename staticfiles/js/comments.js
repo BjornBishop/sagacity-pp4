02139cf1312902f0f28c883032712326ec4fd7c2
Bjorn Bishop <bjornbishop@gmail.com>
2025-02-02 22:43:27 +0000

    added cryptography to requirements.txt

--- staticfiles/js/comments.js
+++ staticfiles/js/comments.js
@@ -6,11 +6,13 @@ document.addEventListener("DOMContentLoaded", function() {
 
     for (let button of editButtons) {
         button.addEventListener("click", (e) => {
+            e.preventDefault();  // Prevent the default action
             let commentId = e.target.getAttribute("comment_id");
             let commentContent = document.getElementById(`comment${commentId}`).innerText;
+            let slug = commentForm.getAttribute("data-slug"); 
             commentText.value = commentContent;
             submitButton.innerText = "Update";
-            commentForm.setAttribute("action", `/assignment/edit_comment/${commentId}`);
+            commentForm.setAttribute("action", `/assignment/${slug}/edit_comment/${commentId}/`); 
         });
     }
 });
