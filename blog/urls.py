a913cfa17d6ec489ac0949e30d7fb4c6016f34c3
Bjorn Bishop <bjornbishop@gmail.com>
2025-02-02 22:06:10 +0000

    comment edit functionality added

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from blog.views import ConsultingAssignmentList, ConsultingAssignmentDetail
+from blog.views import ConsultingAssignmentList, ConsultingAssignmentDetail, comment_edit
 
 urlpatterns = [
     path('', ConsultingAssignmentList.as_view(), name='home'),
     path('assignment/<slug:slug>/', ConsultingAssignmentDetail.as_view(), name='assignment_detail'),
+    path('assignment/<slug:slug>/edit_comment/<int:comment_id>/', comment_edit, name='comment_edit'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views import generic
+from django.urls import reverse
 from django.contrib import messages
-from .models import ConsultingAssignment
+from django.http import HttpResponseRedirect
+from .models import ConsultingAssignment, Comment
 from .forms import CommentForm
 
 class ConsultingAssignmentList(generic.ListView):
@@ -44,3 +46,24 @@ class ConsultingAssignmentDetail(generic.DetailView):
         context = self.get_context_data()
         context['comment_form'] = comment_form
         return self.render_to_response(context)
+
+def comment_edit(request, slug, comment_id):
+    """
+    View to edit comments
+    """
+    if request.method == "POST":
+        queryset = ConsultingAssignment.objects.filter(status__in=[1, 2])
+        post = get_object_or_404(queryset, slug=slug)
+        comment = get_object_or_404(Comment, pk=comment_id)
+        comment_form = CommentForm(data=request.POST, instance=comment)
+
+        if comment_form.is_valid() and comment.author == request.user:
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.approved = False
+            comment.save()
+            messages.add_message(request, messages.SUCCESS, 'Comment Updated!')
+        else:
+            messages.add_message(request, messages.ERROR, 'Error updating comment!')
+
+    return HttpResponseRedirect(reverse('assignment_detail', args=[slug]))
